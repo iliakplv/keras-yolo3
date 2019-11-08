28735bdda1f139034f350ebd7d97c7b92c5630c1
Ilia Kopylov <ilia@springload.co.nz>
2019-11-08 16:12:49 +1300

    Disable anchor generation in training script

--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ wget https://iliakplv-ml-public.s3-ap-southeast-2.amazonaws.com/backend.h5
 wget https://iliakplv-ml-public.s3-ap-southeast-2.amazonaws.com/fish_dataset.zip
 unzip fish_dataset.zip
 
-python3 gen_anchors.py -c config.json
+# python3 gen_anchors.py -c config.json
 
 nohup python3 train.py -c config.json > train.log 2>&1 &
 
